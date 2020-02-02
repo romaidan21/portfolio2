97f94c41df838fcf310ea4d39a1bfcdcdc717cbb
Roman Maidan <romecom2210@gmail.com>
2020-02-03 00:33:28 +0200

    update1.3

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,4 @@
-
-
 document.body.onload = function() {
-
-
 
     let preloader = document.getElementById('page_preloader');
     let r = document.readyState;
@@ -10,20 +6,15 @@ document.body.onload = function() {
 
     if (r === c){
         console.log( 'yep, its good');
-
         setTimeout(()=>{
             preloader.classList.add('done');
             $('.cont-img').removeClass('transform-2x');
-
             setTimeout(()=>{
                 $('.roman-i').removeClass('roman--i--after');
-
                 setTimeout(()=>{
                     $('#myLogo').removeClass('oman-hidden').addClass('oman-active');
                 },1000)
-
             },500)
-
         },1000)
     }
 };
